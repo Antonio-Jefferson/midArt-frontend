fb7299e2c0110e2b6bb328b0b2f26b0760e2ea18
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-05-17 12:43:04 -0300

    feat: add libs react-hook-form and zod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export const metadata = {
 export default function Signin() {
   return (
     <main className="flex bg-slate-50 h-screen">
-      <div className="flex items-center justify-center bg-green-500 h-screen">
-        <div className="w-2/3 bg-red flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center bg-primary h-screen">
+        <div className="w-2/3 flex flex-col items-center justify-center">
           <h1 className="text-center font-bold text-blue-50 text-[34px] m-5">
             SEJA BEM-VINDO
           </h1>
@@ -19,22 +19,24 @@ export default function Signin() {
             conexões na sua jornada criativa.
           </p>
           <Link href={'/sign-up'}>
-            <button className="text-blue-50 font-bold bg-transparent border-spacing-1 border-white">
+            <button className="bg-transparent border-2 border-white rounded-2xl text-white font-semibold px-4 py-2 mt-4 hover:bg-white hover:bg-opacity-10">
               CRIAR CONTA
             </button>
           </Link>
         </div>
       </div>
-      <div className="flex items-center justify-center">
-        <div>
-          <h1>FAÇA SEU LOGIN</h1>
-          <p>
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-2/3 flex flex-col items-center justify-center">
+          <h1 className="text-center font-bold text-primary text-[34px] m-5">
+            FAÇA SEU LOGIN
+          </h1>
+          <p className="text-zinc-500 font-light text-lg">
             Bem-vindo de volta! Insira suas informações de login abaixo para
             acessar sua conta e continuar compartilhando sua arte conosco. Não
             se esqueça de verificar as notificações para ver as novidades da
             nossa comunidade!
           </p>
-          <form>
+          <form className="flex-col">
             <input type="email" />
             <input type="password" />
             <button type="submit">ENTRAR</button>
