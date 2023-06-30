a17b20f667664a7c6bbedac425a476ea5aed9cdb
ElderGr <gbielrs@gmail.com>
2023-06-30 17:34:52 -0300

    feat: code review

--- src/app/dashboard/events/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function EventPage() {
-  return (
-    <div className="w-full bg-primary border border-gray-700 m-auto md:w-[600px] md:min-w-[480px]">
-      <h1>Em breve</h1>
-    </div>
-  )
-}
